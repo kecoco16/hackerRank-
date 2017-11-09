a972554161cfa5f7f3f66370ee5bcd053a4058ce
kecoco <kecoco16@gmail.com>
2017-11-08 22:58:33 -0600

    Update and add day 1 test

--- test/main.js
+++ test/main.js
@@ -1,6 +1,21 @@
-function day1 (inputString) {
-  return `Hello, World.
-  ${inputString}`
+function day0 (inputString) {
+  return `Hello, World.${inputString}`
 }
 
-export { day1 }
+function day1 () {
+  const i = 4
+  const d = 4.0
+  const s = 'HackerRank'
+  let integer
+  let double
+  let string
+  integer = parseInt(12)
+  double = parseFloat(4.0)
+  string = 'is the best place to learn and practice coding!'
+  return `${i + integer} ${(d + double).toFixed(1)} ${s} ${string}`
+}
+
+export { 
+  day0,
+  day1 
+}
