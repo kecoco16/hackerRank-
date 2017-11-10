09e23b5bd18f0a3c9dd71a8ab2f37adab9582f9d
kecoco <kecoco16@gmail.com>
2017-11-10 13:38:36 -0600

    Add day 6 test

---
+++ test/Day6LetsReview.js
@@ -0,0 +1,25 @@
+function day6 (input) {
+  const palabras = input.split(' ')
+  const cantidad = palabras[0]
+  const output = []
+  let par = ''
+  let impar = ''
+  for (let i = 0; i < cantidad; i++) {
+    const palabra = palabras[i + 1]
+    const letras = palabra.split('')
+
+    for (let j = 0; j < palabra.length; j++) {
+      if (j % 2 == 0) {
+        par += letras[j]
+      } else {
+        impar += letras[j]
+      }
+    }
+    output.push(`${par} ${impar}`)
+    par = ''
+    impar = ''
+  }
+  return output
+}
+
+export default day6
