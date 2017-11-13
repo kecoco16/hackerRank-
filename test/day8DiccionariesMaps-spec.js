79ce90aef40112d2171f46e83c928484bbc48fe6
kecoco16 <kecoco16@gmail.com>
2017-11-13 12:08:47 -0600

    Update day8DiccionariesMaps-spec.js
    update

--- test/day8DiccionariesMaps-spec.js
+++ test/day8DiccionariesMaps-spec.js
@@ -6,7 +6,6 @@ import day8 from './day8DiccionariesMaps'
 describe('Day 8 diccionaries maps', () => {
   it('Should if an entry for name is not found, print Not found instead.', () => {
     const input = `3,sam 99912222,tom 11122222,harry 12299933,sam,edward,harry`
-    day8(input)
     const output = ['sam=99912222', 'Not found', 'harry=12299933']
     const response = day8(input)
     expect(response).to.be.an('array')
