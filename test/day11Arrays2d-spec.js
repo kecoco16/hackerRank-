ff3b9874985f58e1649608ca247f9bb51e8423c4
kecoco <kecoco16@gmail.com>
2017-11-14 16:54:40 -0600

    add day 11 test

---
+++ test/day11Arrays2d-spec.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import { expect } from 'chai'
+import day11 from './day11Arrays2d'
+
+describe('Day 11 arrays 2d', () => {
+    it('Should the largest (maximum) hourglass sum found in array.', () => {
+        const input = `1 1 1 0 0 0,0 1 0 0 0 0,1 1 1 0 0 0,0 0 2 4 4 0,0 0 0 2 0 0,0 0 1 2 4 0`
+        const output = 19
+        const response = day11(input)
+        expect(response).to.be.an('number')
+        expect(response).to.be.equal(output)
+    })
+})
---
+++ test/day11Arrays2d.js
@@ -0,0 +1,28 @@
+const getArray = input => {
+  const arr = []
+  for (let i = 0; i < 6; i++) {
+    arr[i] = input[i].split(' ')
+    arr[i] = arr[i].map(Number)
+  }
+  return arr
+}
+
+const getMayor = arr => {
+  let suma = 0
+  let mayor = -999999
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      suma = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+      mayor = (suma > mayor) ? suma : mayor
+    }
+  }
+  return mayor
+}
+
+const day11 = input => {
+  const newInput = input.split(',')
+  const array = getArray(newInput)
+  return getMayor(array)
+}
+
+export default day11
