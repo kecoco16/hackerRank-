2ecdeb4fc06b7b62c399b75c3878735a9dd30486
kecoco <kecoco16@gmail.com>
2017-11-21 13:51:30 -0600

    hacer cambios

--- test/day16Exceptions-spec.js
+++ test/day16Exceptions-spec.js
@@ -1,13 +1,13 @@
 'use strict'
 
 import { expect } from 'chai'
-import day16 from './day16Exceptions'
+import isString from './day16Exceptions'
 
 describe('Day 16 exceptions', () => {
   it('Should bad String if not a number', () => {
     const inputs = ['3', 'test']
-    const isNumber = day16(inputs[0])
-    const notNumber = day16(inputs[1])
+    const isNumber = isString(inputs[0])
+    const notNumber = isString(inputs[1])
     expect(isNumber).to.be.an('string')
     expect(isNumber).to.be.equal(inputs[0])
     expect(notNumber).to.be.an('string')
--- test/day16Exceptions.js
+++ test/day16Exceptions.js
@@ -1,3 +1,3 @@
-const day16 = input => parseInt(input).toString().replace('NaN', 'Bad String')
+const isString = input => parseInt(input).toString().replace('NaN', 'Bad String')
 
-export default day16
+export default isString
